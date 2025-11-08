7c29ae1d553334699db74f0acd059139e4b30bd5
Abhishek Sahoo <75966266+Oreek@users.noreply.github.com>
2025-11-09 03:54:41 +0530

    modified:   components/landing/Hero.tsx 	modified:   components/user/compete/CompetitionRoom.tsx

--- components/landing/Hero.tsx
+++ components/landing/Hero.tsx
@@ -42,7 +42,7 @@ export function Hero() {
           <span>
             Master{' '}
             <span className="rounded-full border border-highlight/25 bg-highlight/50 px-[0.35em] py-[0.125em] text-highlight-depth dark:bg-highlight/85 dark:text-background">
-              DSA
+              Programming
             </span>
           </span>
           <span className="flex flex-wrap items-center justify-center">
